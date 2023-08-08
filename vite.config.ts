95001d5f1e47ec6738d0078d16d2fc42e3509d13
giovanni.tommasi <giovanni.tommasi@mia-platform.eu>
2023-08-08 17:14:41 +0200

    feat: added vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts(),
+    tsConfigPaths(),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: pkg.name,
+      formats: ['es', 'umd'],
+      fileName: (format) => `${pkg.name}.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        'antd',
+        'react',
+        'react-dom',
+      ],
+      output: {
+        globals: {
+          antd: 'antd',
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
