cd05ffeb76d1085216e71b78038ffe483a0bee74
giovanni.tommasi <giovanni.tommasi@mia-platform.eu>
2023-08-09 10:41:24 +0200

    feat: added root dir in jest config

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@
 export default {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "jsdom",
+  roots: [
+    "<rootDir>/src"
+  ],
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
