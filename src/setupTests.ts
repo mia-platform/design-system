ec7420a3dc58af10ff6f074535ad1bbd0c862a97
giovanni.tommasi <giovanni.tommasi@mia-platform.eu>
2023-09-22 16:45:12 +0200

    fix: added jest setup test file

---
+++ src/setupTests.ts
@@ -0,0 +1,11 @@
+/**
+ * Imports the Jest DOM utilities
+ *
+ * It can be used to:
+ * - query the DOM for elements,
+ * - assert that elements have certain properties,
+ * - simulate user events.
+ *
+ * This import should be added to the top of your test files before any other imports.
+ */
+import '@testing-library/jest-dom'
