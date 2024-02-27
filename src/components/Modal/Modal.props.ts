6386c80f55f3db679eba48b646844d15183076dc
Federico Maggi <federico.maggi@mia-platform.eu>
2024-02-27 17:02:15 +0100

    feat(#349): modal isMaskClosable prop

--- src/components/Modal/Modal.props.ts
+++ src/components/Modal/Modal.props.ts
@@ -68,6 +68,11 @@ export type ModalProps = {
   isClosable?: boolean,
 
   /**
+   * Whether the modal is closable with a click on the modal mask.
+   */
+  isMaskClosable?: boolean,
+
+  /**
    * Whether the modal is visible.
    */
   isVisible?: boolean,
