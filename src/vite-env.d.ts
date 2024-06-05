e19aba66e194655f1d74af230c68d1ecdddb3fac
epessina <edoardopessina.priv@gmail.com>
2024-06-05 11:32:38 +0200

    Removed ?react from svg imports

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -16,5 +16,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/// <reference types="vite/client" />
+declare module "*.svg" {
+  import * as React from "react"
+
+  const ReactComponent: React.FunctionComponent<React.ComponentProps<"svg"> & { title?: string }>
+
+  export default ReactComponent
+}
+
 /// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vite/client" />
