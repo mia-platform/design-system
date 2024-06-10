52542db9ffac4b2a30fcf9dd67d522abc46fca44
danielecina <icloud.d@gmail.com>
2024-06-10 10:21:04 +0200

    upgrade: version

--- src/hooks/useModal/useModal.test.tsx
+++ src/hooks/useModal/useModal.test.tsx
@@ -16,10 +16,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { ReactNode, useCallback, useState } from 'react'
 import { act, render, renderHook, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Button } from '../..'
 import { useModal } from '.'
+
+const ModalWrapper = ({ modalTitle, modalChildren } : {modalTitle: string, modalChildren: ReactNode}): JSX.Element => {
+  const { Modal, openModal, toggleModal } = useModal()
+  const [status, setStatus] = useState('idle')
+
+  const changeContent = useCallback(() : void => {
+    setStatus('changed')
+  }, [])
+
+  return (
+    <>
+      <Button onClick={openModal}>Open Modal</Button>
+      <Button onClick={toggleModal}>Toggle Modal Visibility</Button>
+      <Button onClick={changeContent}>Change Modal Content</Button>
+      <Modal title={modalTitle}>
+        {status === 'idle' ? modalChildren : 'Something changed!'}
+      </Modal>
+    </>
+  )
+}
 
 describe('useModal', () => {
   test('should return isModalVisible, openModal, closeModal and toggleModal properties', async() => {
@@ -41,34 +63,37 @@ describe('useModal', () => {
     const title = 'Modal Title'
     const children = 'Modal Content'
 
-    const Example = (): JSX.Element => {
-      const { Modal, openModal, toggleModal } = useModal()
-      return (
-        <>
-          <Button onClick={openModal}>Open Modal</Button>
-          <Button onClick={toggleModal}>Change Modal Status</Button>
-          <Modal title={title}>
-            {children}
-          </Modal>
-        </>
-      )
-    }
+    render(<ModalWrapper modalChildren={children} modalTitle={title} />)
 
-    render(<Example />)
-
-    screen.getByRole('button', { name: /Open Modal/i }).click()
+    userEvent.click(screen.getByRole('button', { name: /open modal/i }))
 
     expect(await screen.findByRole('h4', { name: title })).toBeInTheDocument()
     expect(screen.getByText(children)).toBeInTheDocument()
 
-    screen.getByRole('button', { name: /Close/i }).click()
+    screen.getByRole('button', { name: /close/i }).click()
 
     await waitFor(() => expect(screen.queryByRole('h4', { name: title })).not.toBeInTheDocument())
     expect(screen.queryByText(children)).toBeNull()
 
-    screen.getByRole('button', { name: /Change Modal Status/i }).click()
+    userEvent.click(screen.getByRole('button', { name: /toggle modal visibility/i }))
 
     expect(await screen.findByRole('h4', { name: /modal title/i })).toBeInTheDocument()
     expect(screen.getByText(children)).toBeInTheDocument()
+  })
+
+  test('should not rerender Modal when children change', async() => {
+    const title = 'Modal Title'
+    const children = 'Modal Content'
+
+    render(<ModalWrapper modalChildren={children} modalTitle={title} />)
+
+    userEvent.click(screen.getByRole('button', { name: /toggle modal visibility/i }))
+
+    expect(await screen.findByRole('h4', { name: /modal title/i })).toBeInTheDocument()
+    expect(screen.getByText(children)).toBeInTheDocument()
+
+    const modal = screen.getByRole('dialog')
+    userEvent.click(screen.getByRole('button', { name: /change modal content/i }))
+    expect(modal).toBeInTheDocument()
   })
 })
