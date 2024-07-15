0e625f7f092d242da0558ded6e1e8a183a546019
Giovanna Monti <60603265+GiovannaMonti@users.noreply.github.com>
2024-07-15 11:23:26 +0200

    feat(tag): added tag component (#537)
    * feat(tag): added tag component
    
    * Update CONTRIBUTING.md
    
    * revert changes on contributing
    
    * add: license
    
    * add: license

---
+++ src/components/Tag/Tag.props.ts
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ReactNode } from 'react'
+
+export type TagProps = {
+
+    /**
+     * The main content of the Tag.
+     */
+    children?: ReactNode,
+
+    /**
+     * The color of the Tag.
+     */
+    color?: string,
+
+    /**
+     * Displays the default close icon when set to true. It can also be used to render a custom icon.
+     */
+    closeIcon?: ReactNode,
+
+    /**
+     * Callback executed when Tag is closed. Only available if closeIcon is enabled.
+     */
+    onClose?: () => void,
+
+    /**
+     * Displays the Tag border. Defaults to true.
+     */
+    isBordered?: boolean,
+}
