a3e3df4bdc75fa388606f92ef8a2a9c734fbdefe
Federico Maggi <7142570+fredmaggiowski@users.noreply.github.com>
2024-08-08 18:38:18 +0200

    feat(dropwdown): added Dropdown component (#570)
    * feat(dropdown): first dropdown component implementation
    
    * refactor: changed fn name
    
    * refactor: fixed newline
    
    * docs: better props
    
    * feat: danger ui
    
    * refactor: removed comment
    
    * refactor: extracted eventAdapter
    
    * refactor: innernode oneliner
    
    * rem: unused style
    
    * docs: danger doc
    
    * fix: danger ui
    
    * refactor: module css new line

---
+++ src/components/Dropdown/components/Label/Label.tsx
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ReactElement, useMemo } from 'react'
+import classNames from 'classnames'
+
+import { DropdownItem } from '../../props'
+import styles from '../../dropdown.module.css'
+
+const LABEL_DIVIDER = '·'
+
+type LabelProps = DropdownItem
+
+const Label = ({ danger, label, secondaryLabel }: LabelProps): ReactElement => {
+  const primaryLabelClassName = useMemo(() => classNames(
+    styles.primaryLabel,
+    danger ? styles.danger : undefined
+  ), [danger])
+
+  const secondaryLabelClassName = useMemo(() => classNames(
+    styles.secondaryLabel,
+    danger ? styles.danger : undefined
+  ), [danger])
+
+  return (
+    <div className={styles.labelContainer}>
+      <span className={primaryLabelClassName}>{label}</span>
+      {
+        !secondaryLabel
+          ? null
+          : <>
+            <span className={secondaryLabelClassName}>{LABEL_DIVIDER}</span>
+            <span className={secondaryLabelClassName}>{secondaryLabel}</span>
+          </>
+      }
+    </div>
+  )
+}
+export default Label
