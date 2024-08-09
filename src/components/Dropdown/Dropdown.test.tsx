1190b34e4e260937220793072c0c0224b0f6ab51
Federico Maggi <federico.maggi@mia-platform.eu>
2024-08-09 16:31:56 +0200

    rem: testing playground logs

--- src/components/Dropdown/Dropdown.test.tsx
+++ src/components/Dropdown/Dropdown.test.tsx
@@ -182,12 +182,9 @@ describe('Dropdown Component', () => {
         await screen.findByRole('menuitem', { name: 'Label 1' })
         userEvent.hover(screen.getByRole('menuitem', { name: /^Label 2/i }))
         await screen.findByRole('menuitem', { name: 'Label 2-1' }, { timeout: 10000 })
-
         userEvent.hover(screen.getByRole('menuitem', { name: 'Label 2-2 right' }))
-        screen.logTestingPlaygroundURL()
-        const sub1 = await screen.findByRole('menuitem', { name: 'Label 2-2-1' }, { timeout: 10000 })
-        screen.logTestingPlaygroundURL()
 
+        const sub1 = await screen.findByRole('menuitem', { name: 'Label 2-2-1' }, { timeout: 10000 })
         expect(sub1).toBeInTheDocument()
 
         userEvent.click(sub1)
