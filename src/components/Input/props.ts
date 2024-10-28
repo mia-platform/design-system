40be7cda6370efca7234a97c5bbc26d4de003e95
nicmell <39554602+nicmell@users.noreply.github.com>
2024-10-28 11:03:15 +0100

    feat(Input): Added text Input (#673)
    * sort theme tokens
    
    * update theme tokens
    
    * input
    
    * textarea
    
    * working on test
    
    * done
    
    * fix coverage
    
    * chore(deps-dev): bump @types/react from 18.3.11 to 18.3.12 (#672)
    
    Bumps [@types/react](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/react) from 18.3.11 to 18.3.12.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/react)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/react"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * add license
    
    * files rename
    
    * Update src/components/Input/Input.theme.ts
    
    Co-authored-by: Luca Maltagliati <lmalta95@gmail.com>
    
    * sort theme tokens
    
    * update theme tokens
    
    * input
    
    * textarea
    
    * working on test
    
    * done
    
    * fix coverage
    
    * add license
    
    * files rename
    
    * Update src/components/Input/Input.theme.ts
    
    Co-authored-by: Luca Maltagliati <lmalta95@gmail.com>
    
    * line height fix
    
    * line height fix
    
    * typography change
    
    * Update src/components/Input/Input.theme.ts
    
    Co-authored-by: Federico Maggi <7142570+fredmaggiowski@users.noreply.github.com>
    
    * export input
    
    * renamed htmlType and appearance
    
    * Update src/components/Input/props.ts
    
    Co-authored-by: Luca Maltagliati <lmalta95@gmail.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Nicola Melloni <nicola.melloni@mia-care.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Federico Maggi <7142570+fredmaggiowski@users.noreply.github.com>
    Co-authored-by: Luca Maltagliati <lmalta95@gmail.com>
    Co-authored-by: Daniele Cinà <icloud.d@gmail.com>

---
+++ src/components/Input/props.ts
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ChangeEventHandler } from 'react'
+
+import { Appearance, Type } from './types.ts'
+import { IconComponent } from '../Icon/Icon.props.ts'
+
+export type InputProps = {
+
+  /**
+   * The type of the Input.
+   */
+  appearance?: Appearance
+
+  /**
+   * The html type of Input.
+   */
+  type?: Type
+
+  /**
+   * Whether the input is disabled.
+   */
+  isDisabled?: boolean,
+
+  /**
+   * Whether the input has error.
+   */
+  isError?: boolean
+
+  /**
+   * Whether the input is read-only.
+   */
+  isReadOnly?: boolean
+
+  /**
+   * Whether the input is full width. Defaults to true.
+   */
+  isFullWidth?: boolean
+
+  /**
+   * The left icon for the Input.
+   */
+  iconLeft?: IconComponent
+
+  /**
+   * The right icon for the Input.
+   */
+  iconRight?: IconComponent
+
+  /**
+   * The placeholder for the Input.
+   */
+  placeholder?: string
+
+  /**
+   * The input content value.
+   */
+  value?: string
+
+  /**
+   * The input default value
+   */
+  defaultValue?: string
+
+  /**
+   * If allow to remove input content with clear icon.
+   */
+  allowClear?: boolean
+
+  /**
+   * The maximum number of characters in Input.
+   */
+  maxLength?: number
+
+  /**
+   * The number of rows for the input.
+   */
+  rows?: number
+
+  /**
+   * Callback when user input.
+   */
+  onChange?: ChangeEventHandler;
+}
