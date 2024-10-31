8f4b0f7bb68ff601bf60cf4529b778545b9e2a2f
Nicola Melloni <nicola.melloni@mia-care.io>
2024-10-31 16:53:09 +0100

    input fixes

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -18,7 +18,7 @@
 
 import { Select as AntSelect, SelectProps as AntSelectProps } from 'antd'
 import { PiCaretDown, PiCaretUp, PiCheck } from 'react-icons/pi'
-import React, { ReactElement, useMemo, useState } from 'react'
+import { ReactElement, useMemo, useState } from 'react'
 import classnames from 'classnames'
 
 import { BaseInput, defaults as baseInputDefaults } from '../BaseInput/BaseInput'
