893c8b5464b54bfd01ca5f8a7de49da07f2c0fd6
nicmell <39554602+nicmell@users.noreply.github.com>
2024-11-26 16:15:58 +0100

    feat(Alert): Added Alert component (#729)
    * alert component
    
    * alert component
    
    * added comment in prop
    
    ---------
    
    Co-authored-by: Nicola Melloni <nicola.melloni@mia-care.io>

---
+++ src/components/Alert/Alert.test.tsx
@@ -0,0 +1,153 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { PiStar } from 'react-icons/pi'
+
+import { Alert } from './Alert'
+import { Button } from '../Button'
+
+describe('Alert Snapshot Tests', () => {
+  it('renders Special correctly', () => {
+    const { asFragment } = render(<Alert description="Description" title="Title" />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders Info correctly', () => {
+    const { asFragment } = render(<Alert description="Description" title="Title" type={Alert.Type.Info} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders Warning correctly', () => {
+    const { asFragment } = render(<Alert description="Description" title="Title" type={Alert.Type.Warning} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders Error correctly', () => {
+    const { asFragment } = render(<Alert description="Description" title="Title" type={Alert.Type.Error} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders Success correctly', () => {
+    const { asFragment } = render(<Alert description="Description" title="Title" type={Alert.Type.Success} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders InfoCompressed correctly', () => {
+    const { asFragment } = render(
+      <Alert
+        description="Description"
+        isCompressed={true}
+        title="Title"
+        type={Alert.Type.Info}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders WarningCompressed correctly', () => {
+    const { asFragment } = render(
+      <Alert
+        description="Description"
+        isCompressed={true}
+        title="Title"
+        type={Alert.Type.Warning}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders ErrorCompressed correctly', () => {
+    const { asFragment } = render(
+      <Alert
+        description="Description"
+        isCompressed={true}
+        title="Title"
+        type={Alert.Type.Error}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders SuccessCompressed correctly', () => {
+    const { asFragment } = render(
+      <Alert
+        description="Description"
+        isCompressed={true}
+        title="Title"
+        type={Alert.Type.Success}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders SpecialCompressed correctly', () => {
+    const { asFragment } = render(
+      <Alert description="Description" isCompressed={true} title="Title" />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders WithAction correctly', () => {
+    const { asFragment } = render(
+      <Alert
+        action={<Button>Action</Button>}
+        description="Description"
+        title="Title"
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders WithCustomIcon correctly', () => {
+    const { asFragment } = render(
+      <Alert description="Description" icon={PiStar} title="Title" />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders WithCustomContent correctly', () => {
+    const { asFragment } = render(
+      <Alert isCompressed={true}>
+        <ul style={{ margin: '8px 24px' }}>
+          <li>Custom content</li>
+          <li>Custom content</li>
+        </ul>
+      </Alert>
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should call onClose if the close button is pressed', async() => {
+    const onClose = jest.fn()
+    render(
+      <Alert
+        description="Description"
+        title="Title"
+        onClose={onClose}
+      />
+    )
+
+    const closeButton = screen.getByRole('button')
+    fireEvent.click(closeButton)
+
+    await waitFor(() => {
+      expect(onClose).toHaveBeenCalled()
+    }, { timeout: 15000 })
+  })
+})
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,137 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  PiCheckCircleFill,
+  PiCircleHalfTiltFill,
+  PiInfoFill,
+  PiWarningFill,
+  PiX,
+  PiXCircleFill,
+} from 'react-icons/pi'
+import { ReactElement, useMemo } from 'react'
+import { Alert as AntAlert } from 'antd'
+import classnames from 'classnames'
+
+import { AlertProps } from './props.ts'
+import CheckCircle from '../../assets/icons/CheckCircle.svg'
+import ICircle from '../../assets/icons/ICircle.svg'
+import { Icon } from '../Icon'
+import { IconComponent } from '../Icon/Icon.props.ts'
+import MiExclamationCircle from '../../assets/icons/ExclamationCircle.svg'
+import { Type } from './types.ts'
+import XCircle from '../../assets/icons/XCircle.svg'
+import styles from './Alert.module.css'
+
+export const defaults = {
+  isClosable: true,
+}
+
+const getIcon = (type?: Type): IconComponent => {
+  switch (type) {
+  case Type.Info:
+    return PiInfoFill
+  case Type.Warning:
+    return PiWarningFill
+  case Type.Error:
+    return PiXCircleFill
+  case Type.Success:
+    return PiCheckCircleFill
+  default:
+    return PiCircleHalfTiltFill
+  }
+}
+
+const getIconCompressed = (type?: Type): IconComponent => {
+  switch (type) {
+  case Type.Info:
+    return ICircle
+  case Type.Warning:
+    return MiExclamationCircle
+  case Type.Error:
+    return XCircle
+  case Type.Success:
+    return CheckCircle
+  default:
+    return PiCircleHalfTiltFill
+  }
+}
+
+export const Alert = ({
+  title: titleProp,
+  description,
+  isClosable = defaults.isClosable,
+  type,
+  children,
+  isCompressed,
+  icon,
+  action,
+  onClose,
+}: AlertProps): ReactElement => {
+  const className = useMemo(() => classnames([
+    styles.alert,
+    type && styles[type],
+    isCompressed && styles.compressed,
+  ]), [isCompressed, type])
+
+  const title = useMemo(() => {
+    return isCompressed
+      ? (
+        <div className={styles.titleContainer}>
+          <Icon component={icon || getIconCompressed(type)} size={16} />
+          <span className={styles.title}>{titleProp}</span>
+        </div>
+      )
+      : (
+        <span className={styles.title}>{titleProp}</span>
+      )
+  }, [icon, isCompressed, titleProp, type])
+
+  const content = useMemo(() => {
+    return (description || children) && (
+      <div>
+        {description && (
+          <span className={styles.description}>{description}</span>
+        )}
+        {children && (
+          <div className={styles.content}>{children}</div>
+        )}
+      </div>
+    )
+  }, [children, description])
+
+  const closable = useMemo(() => {
+    return !isCompressed && isClosable && { closeIcon: <Icon component={PiX} size={16} /> }
+  }, [isCompressed, isClosable])
+
+  return (
+    <AntAlert
+      action={action}
+      className={className}
+      closable={closable}
+      description={content}
+      icon={<Icon component={icon || getIcon(type)} />}
+      message={title}
+      showIcon={!isCompressed}
+      type={type}
+      onClose={onClose}
+    />
+  )
+}
+
+Alert.Type = Type
