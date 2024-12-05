ed839be46735f31f2c0cc3fa799f96ba03cf9fa5
Federico Maggi <federico.maggi@mia-platform.eu>
2024-12-05 19:01:12 +0100

    poc(#743): story for stopPropagation

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -22,7 +22,7 @@ import { action } from '@storybook/addon-actions'
 
 import { Button } from '../Button'
 import { Dropdown } from '.'
-import { DropdownProps } from './props'
+import { DropdownClickEvent, DropdownProps } from './props'
 import { Icon } from '../Icon'
 import { Placement } from './types'
 import { Tag } from '../Tag'
@@ -193,4 +193,19 @@ export const SelectionIsNotPersisted: Story = {
   args: {
     persistSelection: false,
   },
+}
+
+export const DropdownOverClickableItem: Story = {
+  decorators: [(_, { args }) => {
+    return (
+      <div style={{ 'background': 'red', height: 500 }} onClick={action('div below clicked!!')}>
+        <Dropdown
+          {...args}
+          onClick={(event: DropdownClickEvent) => {
+            event.domEvent.stopPropagation()
+          }}
+        />
+      </div>
+    )
+  }],
 }
