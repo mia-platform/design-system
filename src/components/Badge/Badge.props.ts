c81a62c20943b943f833cd44d7be679ce50ed0f4
Federico Pini <17337493+fedepini@users.noreply.github.com>
2024-12-16 16:52:03 +0100

    feat: add Badge component (#767)
    * feat: added Badge component
    
    * feat: updated props and fixed style
    
    * feat: updated style
    
    * refactor: renamed prop
    
    * refactor: updated css prop  order
    
    * feat: added tests
    
    * refactor: updated story
    
    * feat: exported Badge component
    
    * chore: added licens
    
    ---------
    
    Co-authored-by: federico.pini <federico.pini@mia-platform.eu>

---
+++ src/components/Badge/Badge.props.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ReactNode } from 'react'
+
+import { IconComponent } from '../Icon/Icon.props'
+
+export type BadgeProps = {
+
+  /**
+   * Optional description, it displays formatted text customized content below the badge title.
+   */
+  description?: string | ReactNode;
+
+  /**
+   * Additional content to be displayed on the right side of the badge.
+   */
+  extra?: ReactNode;
+
+  /**
+   * The icon component to render within the badge.
+   */
+  icon: IconComponent;
+
+  /**
+   * The main title text, it displays formatted text or customized content within the badge.
+   */
+  title: string | ReactNode;
+
+  /**
+   * Additional content to be displayed alongside the badge title (e.g., a tag or an icon).
+   */
+  titleExtra?: ReactNode;
+};
---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2024 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ReactElement, useMemo } from 'react'
+import { TooltipProps } from 'antd'
+
+import { BadgeProps } from './Badge.props'
+import { Icon } from '../Icon'
+import { Typography } from '../Typography'
+import styles from './Badge.module.css'
+import { useTheme } from '../../hooks/useTheme'
+
+const tooltipProps: TooltipProps = {
+  getPopupContainer: (triggerNode) => triggerNode.parentElement!,
+  getTooltipContainer: (triggerNode) => triggerNode.parentElement!,
+  destroyTooltipOnHide: true,
+  overlayClassName: styles.tooltip,
+  placement: 'bottom',
+}
+
+/**
+ * UI component for displaying information about a resource, emphasizing some of its main features.
+ * @returns {ReactElement} Badge component
+ */
+export const Badge = ({
+  description: customDescription,
+  extra: customExtra,
+  icon: customIcon,
+  title: customTitle,
+  titleExtra,
+}: BadgeProps): ReactElement => {
+  const { palette } = useTheme()
+
+  const icon = useMemo(() => (
+    <div className={styles.icon} data-testid="badge-icon">
+      <Icon color={palette.text.neutral.subtle} component={customIcon} size={48} />
+    </div>
+  ), [customIcon, palette.text.neutral.subtle])
+
+  const title = useMemo(() => (
+    <div className={styles.title}>
+      <Typography.H3 ellipsis={{ rows: 1, tooltip: { ...tooltipProps, title: customTitle } }}>
+        {customTitle}
+      </Typography.H3>
+      {titleExtra}
+    </div>
+  ), [customTitle, titleExtra])
+
+  const description = useMemo(() => {
+    if (!customDescription) { return }
+
+    return (
+      <Typography.BodyS ellipsis={{ rows: 1, tooltip: { ...tooltipProps, title: customDescription } }}>
+        {customDescription}
+      </Typography.BodyS>
+    )
+  }, [customDescription])
+
+  const extra = useMemo(() => {
+    if (!customExtra) { return }
+
+    return (
+      <div className={styles.extra}>
+        {customExtra}
+      </div>
+    )
+  }, [customExtra])
+
+  return (
+    <div className={styles.badge}>
+      {icon}
+      <div className={styles.content}>
+        {title}
+        {description}
+      </div>
+      {extra}
+    </div>
+  )
+}
