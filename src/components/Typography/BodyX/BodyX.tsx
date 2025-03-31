ac9c01b0d5cfc3c49588d7a1bfc7709467158569
FrankieBortot <bortot.frankie@gmail.com>
2025-03-31 15:05:01 +0200

    Update src/components/Typography/BodyX/BodyX.tsx
    Co-authored-by: Daniele Cinà <icloud.d@gmail.com>

--- src/components/Typography/BodyX/BodyX.tsx
+++ src/components/Typography/BodyX/BodyX.tsx
@@ -68,7 +68,7 @@ export const BodyX = ({
 
   return (
     <AntParagraph
-      className= {bodyClassName}
+      className={bodyClassName}
       copyable={copyable}
       ellipsis={ellipsis}
       role={'paragraph'}
